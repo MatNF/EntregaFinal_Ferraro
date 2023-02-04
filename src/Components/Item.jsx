cd407db064abc716e79cc13a753a556311ea94b4
Matías Ferraro <matiasferraro@Matiass-MacBook-Pro.local>
2023-02-04 18:17:43 -0300

    modifico item, itemlist y estilos git status

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const Item = ({ item }) => {
   return (
-    <div>
-      <h3>{item.title}</h3>
-      <p>{item.description}</p>
-      <p>Price: ${item.price}</p>
-      <img src={item.pictureUrl} alt={item.title} />
+    <div className="counter">
+      <div className="infoProducto">
+        <div className="titleProd">
+          <h3>{item.title}</h3>
+        </div>
+        <div className="moreInfo">
+          <p>{item.description}</p>
+          <p>Price: ${item.price}</p>
+        </div>
+      </div>
+
+      <div className="imagenProducto">
+        <img src={item.pictureUrl} alt={item.title} />
+      </div>
     </div>
   );
 };
--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Item from "./Item";
+import "./styles/hola.css"
 
 const ItemList = ({ items }) => {
   return (
-    <div>
+    <div className="containerItemList">
       {items.map((item) => (
         <Item key={item.id} item={item} />
       ))}
